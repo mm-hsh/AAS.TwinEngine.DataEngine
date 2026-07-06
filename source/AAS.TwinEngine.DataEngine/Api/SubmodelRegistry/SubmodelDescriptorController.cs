f8c9f4df1e89e71c8fccfab9714c4ef46b2852a9
Hardi Shah <hsh@mm-software.com>
2026-07-06 10:24:20 +0530

    Enahnce DataEngine Swagger Endpoints Description (#158)

--- source/AAS.TwinEngine.DataEngine/Api/SubmodelRegistry/SubmodelDescriptorController.cs
+++ source/AAS.TwinEngine.DataEngine/Api/SubmodelRegistry/SubmodelDescriptorController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.ComponentModel;
+using System.Net;
 
 using AAS.TwinEngine.DataEngine.Api.SubmodelRegistry.Handler;
 using AAS.TwinEngine.DataEngine.Api.SubmodelRegistry.Requests;
@@ -9,16 +10,28 @@ using Asp.Versioning;
 
 using Microsoft.AspNetCore.Mvc;
 
+using NSwag.Annotations;
+
 namespace AAS.TwinEngine.DataEngine.Api.SubmodelRegistry;
 
 [ApiController]
 [Route("submodel-descriptors")]
+[OpenApiTags("Submodel Registry API")]
 [ApiVersion(1)]
 public class SubmodelDescriptorController(
     ILogger<SubmodelDescriptorController> logger,
     ISubmodelDescriptorHandler submodelDescriptorHandler)
     : ControllerBase
 {
+    /// <summary>
+    /// Returns a specific Submodel Descriptor.
+    /// </summary>
+    /// <param name="submodelIdentifier">The Submodel’s unique id (UTF8-BASE64-URL-encoded)</param>
+    /// <param name="cancellationToken">Cancellation token</param>
+    /// <response code="200">Requested Submodel Descriptor</response>
+    /// <response code="400">Bad Request, e.g.the request parameters of the format of the request body is wrong.</response>
+    /// <response code="404">Not Found</response>
+    /// <response code="500">Internal Server Error</response>
     [HttpGet("{submodelIdentifier}")]
     [ProducesResponseType(typeof(SubmodelDescriptorDto), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(ServiceErrorResponse), (int)HttpStatusCode.BadRequest)]
--- source/AAS.TwinEngine.DataEngine/Api/SubmodelRepository/SerializationController.cs
+++ source/AAS.TwinEngine.DataEngine/Api/SubmodelRepository/SerializationController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.ComponentModel;
+using System.Net;
 
 using AAS.TwinEngine.DataEngine.Api.SubmodelRepository.Handler;
 using AAS.TwinEngine.DataEngine.Api.SubmodelRepository.Requests;
@@ -8,15 +9,29 @@ using Asp.Versioning;
 
 using Microsoft.AspNetCore.Mvc;
 
+using NSwag.Annotations;
+
 namespace AAS.TwinEngine.DataEngine.Api.SubmodelRepository;
 
 [ApiController]
 [Route("serialization")]
 [ApiVersion(1)]
+[OpenApiTags("Serialization API")]
 public class SerializationController(
     ILogger<SerializationController> logger,
     ISerializationHandler serializationHandler) : ControllerBase
 {
+    /// <summary>
+    /// Returns an appropriate serialization based on the specified format(see SerializationFormat)
+    /// </summary>
+    /// <param name="aasIds">The Asset Administration Shells' unique ids (UTF8-BASE64-URL-encoded)</param>
+    /// <param name="submodelIds">The Submodels' unique ids (UTF8-BASE64-URL-encoded)</param>
+    /// <param name="cancellationToken">Cancellation token.</param>
+    /// <param name="includeConceptDescriptions">Include Concept Descriptions?</param>
+    /// <response code="200">Requested serialization based on SerializationFormat</response>
+    /// <response code="400">Bad Request, e.g.the request parameters of the format of the request body is wrong.</response>
+    /// <response code="404">Not Found</response>
+    /// <response code="500">Internal Server Error</response>
     [HttpGet("")]
     [ProducesResponseType(typeof(FileStreamResult), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(ServiceErrorResponse), (int)HttpStatusCode.NotFound)]
