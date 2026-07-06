a2282cacc885389d6c56239a61c6a2c7f4b8b069
Hardi Shah <hsh@mm-software.com>
2026-07-06 10:24:20 +0530

    Enahnce DataEngine Swagger Endpoints Description (#158)

--- source/AAS.TwinEngine.DataEngine/Api/Discovery/Requests/AssetLinkDto.cs
+++ source/AAS.TwinEngine.DataEngine/Api/Discovery/Requests/AssetLinkDto.cs
@@ -1,4 +1,5 @@
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 using System.Text.Json.Serialization;
 
 namespace AAS.TwinEngine.DataEngine.Api.Discovery.Requests;
@@ -7,9 +8,11 @@ public class AssetLinkDto
 {
     [JsonPropertyName("name")]
     [Required]
+    [DefaultValue("ud800MudbffAudbffJudbff7ud800d3MnAudbff@udbff:>Zudbffbud800ZU^=L")]
     public required string Name { get; set; }
 
     [JsonPropertyName("value")]
     [Required]
+    [DefaultValue("8ud800Gudbffjudbff4lDVd9tud800phYud8001udbffqud800Pud800ssPWFD:hud800CAOud800MO<WT")]
     public required string Value { get; set; }
 }
--- source/AAS.TwinEngine.DataEngine/Api/SubmodelRepository/SubmodelRepositoryController.cs
+++ source/AAS.TwinEngine.DataEngine/Api/SubmodelRepository/SubmodelRepositoryController.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.ComponentModel;
+using System.Net;
 using System.Text.Json.Nodes;
 
 using AAS.TwinEngine.DataEngine.Api.SubmodelRepository.Handler;
@@ -11,18 +12,29 @@ using Asp.Versioning;
 
 using Microsoft.AspNetCore.Mvc;
 
+using NSwag.Annotations;
+
 namespace AAS.TwinEngine.DataEngine.Api.SubmodelRepository;
 
 [ApiController]
 [Route("submodels")]
 [ApiVersion(1)]
+[OpenApiTags("Submodel Repository API")]
 public class SubmodelRepositoryController(
     ILogger<SubmodelRepositoryController> logger,
     ISubmodelRepositoryHandler submodelRepositoryHandler)
     : ControllerBase
 {
+    /// <summary>
+    /// Returns a specific Submodel.
+    /// </summary>
+    /// <param name="submodelIdentifier">The Submodel's unique id (UTF8-BASE64-URL-encoded)</param>
+    /// <response code="200">Requested Submodel</response>
+    /// <response code="400">Bad Request, e.g.the request parameters of the format of the request body is wrong.</response>
+    /// <response code="404">Not Found</response>
+    /// <response code="500">Internal Server Error</response>
     [HttpGet("{submodelIdentifier}")]
-    [ProducesResponseType(typeof(ISubmodel), (int)HttpStatusCode.OK)]
+    [ProducesResponseType(typeof(Submodel), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(ServiceErrorResponse), (int)HttpStatusCode.BadRequest)]
     [ProducesResponseType(typeof(ServiceErrorResponse), (int)HttpStatusCode.NotFound)]
     [ProducesResponseType(typeof(ServiceErrorResponse), (int)HttpStatusCode.InternalServerError)]
@@ -34,6 +46,15 @@ public class SubmodelRepositoryController(
         return Ok(Jsonization.Serialize.ToJsonObject(response));
     }
 
+    /// <summary>
+    /// Returns a specific submodel element from the submodel at a specified path
+    /// </summary>
+    /// <param name="submodelIdentifier">The Submodel's unique id (UTF8-BASE64-URL-encoded)</param>
+    /// <param name="idShortPath">The IdShort path to the submodel element (dot-separated)</param>
+    /// <response code="200">Requested submodel element</response>
+    /// <response code="400">Bad Request, e.g.the request parameters of the format of the request body is wrong.</response>
+    /// <response code="404">Not Found</response>
+    /// <response code="500">Internal Server Error</response>
     [HttpGet("{submodelIdentifier}/submodel-elements/{idShortPath}")]
     [ProducesResponseType(typeof(ISubmodelElement), (int)HttpStatusCode.OK)]
     [ProducesResponseType(typeof(ServiceErrorResponse), (int)HttpStatusCode.BadRequest)]
